9bcc51cd00fb0396f357f521fbd158d9a8b997a3
Chris Lee <dkrj45@gmail.com>
2022-09-11 18:41:04 -0400

    form validation fully functional

--- front-end/src/pages/SignupPage/SignupPage.js
+++ front-end/src/pages/SignupPage/SignupPage.js
@@ -4,40 +4,79 @@ import { useRef, useState } from "react";
 
 function SignupPage() {
 
-  let navigate = useNavigate()
+  let navigate = useNavigate();
 
-  const formRef = useRef()
-  const form = formRef.current
+  const formRef = useRef();
+  const form = formRef.current;
 
-  const [email, setEmail] = useState('')
-  const [password, setPassword] = useState('')
-  const [confirmPassword, setConfirmPassword] = useState('')
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
 
   const changeEmail=()=>{
-    setEmail(form.email.value)
+    setEmail(form.email.value);
   }
 
   const changePassword=()=>{
-    setPassword(form.password.value)
+    setPassword(form.password.value);
   }
 
   const changeConfirmPassword=()=>{
-    setConfirmPassword(form.confirmPassword.value)
+    setConfirmPassword(form.confirmPassword.value);
   }
 
   const isEmailValid=()=>{
     if(!email){
-      console.log('false')
+      document.querySelector(".signuppage__form--email").classList.remove("signuppage__form--email--error")
+      if(document.querySelector(".signuppage__error-message--email")){
+        document.querySelector(".signuppage__error-message--email").remove()
+      }
       return false
     }
     if(!(email.includes('@')) || !(email.includes('.'))){
-      document.querySelector(".signuppage__form--email").classList.add("signuppage__form--email--error")
+      document.querySelector(".signuppage__form--email").classList.add("signuppage__form--email--error");
+      if(!(document.querySelector(".signuppage__error-message--email"))){
+        let emailLabel = document.getElementById("label__email")
+        let emailErrorMessage = document.createElement("h3");
+        emailErrorMessage.classList.add("signuppage__error-message--email")
+        emailErrorMessage.innerText = "Please enter a valid email address."
+        emailLabel.appendChild(emailErrorMessage);
+      }
       return false;
     } else {
       document.querySelector(".signuppage__form--email").classList.remove("signuppage__form--email--error")
+      if(document.querySelector(".signuppage__error-message--email")){
+        document.querySelector(".signuppage__error-message--email").remove()
+      }
       return true;
     }
-    
+  }
+
+  const isConfirmPasswordValid=()=>{
+    if(!confirmPassword){
+      document.querySelector(".signuppage__form--confirm-password").classList.remove("signuppage__form--confirm-password--error")
+      if(document.querySelector(".signuppage__error-message--confirm-password")){
+        document.querySelector(".signuppage__error-message--confirm-password").remove()
+      }
+      return false
+    }
+    else if(confirmPassword!==password){
+      document.querySelector(".signuppage__form--confirm-password").classList.add("signuppage__form--confirm-password--error")
+      if(!(document.querySelector(".signuppage__error-message--confirm-password"))){
+        let confirmPasswordLabel = document.getElementById("label__confirm-password")
+        let confirmPasswordErrorMessage = document.createElement("h3");
+        confirmPasswordErrorMessage.classList.add("signuppage__error-message--confirm-password")
+        confirmPasswordErrorMessage.innerText = "Your passwords are not matching."
+        confirmPasswordLabel.appendChild(confirmPasswordErrorMessage);
+      }
+      return false
+    } else {
+      document.querySelector(".signuppage__form--confirm-password").classList.remove("signuppage__form--confirm-password--error")
+      if(document.querySelector(".signuppage__error-message--confirm-password")){
+        document.querySelector(".signuppage__error-message--confirm-password").remove()
+      }
+      return true;
+    }
   }
 
   const clickHandler = (e) => {
@@ -47,14 +86,12 @@ function SignupPage() {
 
     if(name==="" || email==="" || password==="" || confirmPassword===""){
       alert("please fill in all the fields.")
-    } else if(!(email.includes('@')) || !(email.includes('.'))){
+    } else if(!isEmailValid()){
       alert('Please enter a valid email.')
     }
-      else if(password!==confirmPassword){
+      else if(!isConfirmPasswordValid()){
       alert("Passwords do not match. Please try again.")
-      document.querySelector(".signuppage__form--confirm-password").classList.add("signuppage__form--confirm-password--error")
     } else{
-      document.querySelector(".signuppage__form--confirm-password").classList.remove("signuppage__form--confirm-password--error")
       alert("Your account has been successfully created! Please log in with your new account.")
       navigate('/Loginpage')
     }
@@ -72,7 +109,7 @@ function SignupPage() {
                     <span>Name:</span>
                     <input className='signuppage__form--name' type='text' name='name' placeholder='name'></input>
                 </label>
-                <label className='signuppage__form--label'>
+                <label id='label__email' className='signuppage__form--label'>
                     <span>Email:</span>
                     <input onChange={changeEmail} onBlur={isEmailValid} className='signuppage__form--email' type='email' name='email' placeholder='email@website.com'></input>
                 </label>
@@ -80,9 +117,9 @@ function SignupPage() {
                     <span>Password:</span>
                     <input onChange={changePassword} className='signuppage__form--password' type='text' name='password' placeholder='password'></input>
                 </label>
-                <label className='signuppage__form--label'>
+                <label id='label__confirm-password' className='signuppage__form--label'>
                     <span>Confirm Password:</span>
-                    <input onChange={changeConfirmPassword} id="confirmPassword" className='signuppage__form--confirm-password' type='text' name='confirmPassword' placeholder='confirm password'></input>
+                    <input onChange={changeConfirmPassword} onBlur={isConfirmPasswordValid} id="confirmPassword" className='signuppage__form--confirm-password' type='text' name='confirmPassword' placeholder='confirm password'></input>
                 </label>
                 <button onClick={clickHandler} className='signuppage__form--button'>Create Account</button>
             </form>
