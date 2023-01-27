398f9c7c1b365b264ef6637c7ac02aaf02444a1e
Chris Lee <dkrj45@gmail.com>
2023-01-27 00:34:53 -0500

    postgresql database and table created, connected to nodejs

--- back-end/routes/authRouter.js
+++ back-end/routes/authRouter.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const validateForm = require('../controllers/validateForm');
 const router = express.Router();
+const pool = require("../db");
+const bcrypt = require("bcrypt");
 
 router.post("/login", (req, res) => {
     validateForm(req,res)
